54a47a3deb6e8a1e172b282f2b406a3d259aad89
weyewe <w.yunnal@gmail.com>
2011-08-24 14:14:46 +0700

    the first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewStepdo::Application.config.secret_token = '620160d3a9e2b26be7900a10c0f44af353d91f68ec0e347649cb6956a329267ed1e1affd392595d7e29c0f99d0cdc7b68329e00c6b479a58ca0a8cfecbc35aaf'
